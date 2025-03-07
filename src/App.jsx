af5b98c32b03c843edb2441b087384f17773f340
Noa.O <noa.obringer@gmail.com>
2025-03-07 08:33:32 +0100

    init: files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
 
+//Import components
+
+import Header from './components/header'
+
 function App() {
 
   return (
     <>
+    <Header />
     <h1>Projet Lancé</h1>
     </>
   )
---
+++ src/components/header.jsx
@@ -0,0 +1,26 @@
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <h2>Emotion Path</h2>
+        </div>
+
+        <div>
+            <ul>
+                <li>
+                    <a href="#nostalgia">nostalgia</a>
+                </li>
+                <li>
+                    <a href="#uncomfortable">uncomfortable</a>  
+                </li>
+                <li>
+                    <a href="#frustration">frustration</a>
+                </li>
+            </ul>
+        </div>
+
+    </header>
+  );
+}
+
+export default Header;
