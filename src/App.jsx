09713c2cbe6983d8ae7de2e98a4144e63a252266
Noa.O <noa.obringer@gmail.com>
2025-03-07 10:11:19 +0100

    feat: noise

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,30 @@ import './css/global.css'
 
 
 import Homepage from './components/homepage'
-
+import Noise from './components/Noise'
 
 
 function App() {
 
   return (
     <>
+    <div style={{width: '100%', height: '100%', position: 'absolute', overflow: 'hidden'}}>
+
+      <Noise
+
+        patternSize={250}
+
+        patternScaleX={2}
+
+        patternScaleY={2}
+
+        patternRefreshInterval={2}
+
+        patternAlpha={15}
+
+      />
+
+    </div>
     <Homepage />
     </>
   )
