4dcc321b7e3424bd16d1bd1422dc584db240f7be
Noa.O <noa.obringer@gmail.com>
2025-03-11 16:43:20 +0100

    feat: transition section

--- src/App.jsx
+++ src/App.jsx
@@ -9,40 +9,36 @@ import Nostalgia from './components/Nostalgia'
 import NostalgiaTransition from './components/NostalgiaTransition'
 import CanvasLightEffect from './components/CanvasLight'
 
-
-
 function App() {
-
   return (
     <>
-    <Loader />
+      <Loader />
 
-    <div style={{width: '100%', height: '100%', position: 'absolute', overflow: 'hidden'}}>
-
-      <Noise
-
-        patternSize={250}
-
-        patternScaleX={3}
-
-        patternScaleY={3}
-
-        patternRefreshInterval={2}
-
-        patternAlpha={15}
-
-      />
-
-    </div>
-    <Homepage />
-    <HomeTransition />
-    <div className="nostalgia-container">
-      <Nostalgia />
-    </div>
-
-    <NostalgiaTransition />
-
-    <CanvasLightEffect />
+      <div style={{width: '100%', height: '100%', position: 'absolute', overflow: 'hidden'}}>
+        <Noise
+          patternSize={250}
+          patternScaleX={3}
+          patternScaleY={3}
+          patternRefreshInterval={2}
+          patternAlpha={15}
+        />
+      </div>
+      
+      <Homepage />
+      <HomeTransition />
+      
+      
+      <div className="nostalgia-container">
+        <Nostalgia />
+      </div>
+      
+      <div className="section-blend blend-artistic"></div>
+      
+      <NostalgiaTransition />
+      
+      <div className="section-blend blend-glow"></div>
+      
+      <CanvasLightEffect />
     </>
   )
 }
